1d0eba093e279fc1fdbd5f84e4e4ebda74883b39
Vidit Jha <jhavidit@gmail.com>
2020-12-04 18:45:55 +0530

    Added - Adapter class

---
+++ app/src/main/java/tech/jhavidit/redcarpetupassignment/adapter/NewsHeadlinesAdapter.kt
@@ -0,0 +1,84 @@
+package tech.jhavidit.redcarpetupassignment.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.news_item.view.*
+import tech.jhavidit.redcarpetupassignment.R
+import tech.jhavidit.redcarpetupassignment.model.Article
+
+class NewsHeadlinesAdapter(private val context: Context) :
+    RecyclerView.Adapter<NewsHeadlinesAdapter.ViewHolder>() {
+
+    private var list: List<Article> = ArrayList()
+
+    fun setMusicItem(list: List<Article>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.news_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var item = list[position]
+        holder.articleHeadline.text = item.title
+        holder.articleAuthor.text = item.author
+        val publish = "Published at : " + item.publishedAt
+        holder.date.text = publish
+        Glide.with(context)
+            .load(item.urlToImage)
+            .into(holder.articleImage)
+        holder.news.setOnClickListener {
+            val bundle = bundleOf(
+                "author" to item.author,
+                "description" to item.description,
+                "title" to item.title,
+                "source" to item.source,
+                "url" to item.url,
+                "photo" to item.urlToImage,
+                "date" to item.publishedAt
+
+
+            )
+            it.findNavController()
+                .navigate(R.id.action_homeFragment_to_detailsFragment, bundle)
+
+        }
+
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val news: CardView = view.news
+        val articleHeadline: TextView = view.heading
+        val articleImage: ImageView = view.article_photo
+        val articleAuthor: TextView = view.author
+        val date: TextView = view.date
+
+    }
+}
